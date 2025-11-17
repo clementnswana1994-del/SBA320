32a1723bd1599563f8724dfdbdda3639aa623252
clementnswana1994-del <clementnswana1994@gmail.com>
2025-11-17 01:39:35 -0500

    final commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
